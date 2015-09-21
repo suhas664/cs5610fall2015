733fa7f4c4bbda9cea64c5477aa674b290ba0590
Suhas Venkatesha Murthy <venkateshamurthy.s@husky.neu.edu>
2015-09-21 13:26:20 -0400

    changed port to 8080

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ var app = express();
 app.use(express.static(__dirname + '/public'));
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IPi || '127.0.0.1';
-var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+var port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
 app.listen(port, ipaddress);
