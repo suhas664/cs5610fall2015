160bb16fb312efce7dd46580afd0a2c15cab3013
Suhas Venkatesha Murthy <venkateshamurthy.s@husky.neu.edu>
2015-11-18 23:56:54 -0500

    hw4 register redirect works now

--- public/assignment/client/views/register/register.controller.js
+++ public/assignment/client/views/register/register.controller.js
@@ -12,7 +12,7 @@
 
 		$scope.register = function() {
 			console.log("Getting register done");
-			UserService.createUser($scope.user, function(new_user){
+			UserService.createUser($scope.user).then(function(new_user){
 				$rootScope.currentUser = new_user;
 				$location.url('/profile');
 			});
